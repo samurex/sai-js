b8c468320beb6dfa95db9b7b425e9ad2f86cceea
Maciej Samoraj <maciej.samoraj@surferseo.com>
2024-05-15 21:19:22 +0200

    [ui/authorization] feat: sentry error tracking

--- ui/authorization/src/plugins/index.ts
+++ ui/authorization/src/plugins/index.ts
@@ -6,6 +6,7 @@
 
 // Types
 import type { App } from 'vue';
+import * as Sentry from '@sentry/vue';
 
 // Plugins
 import { loadFonts } from './webfontloader';
@@ -14,7 +15,19 @@ import { fluent } from './fluent';
 import pinia from '../store';
 import router from '../router';
 
+function loadSentry(app: App) {
+  const sentryDsn = import.meta.env.VITE_SENTRY_DSN;
+  if (sentryDsn) {
+    Sentry.init({
+      app,
+      dsn: sentryDsn,
+      integrations: [Sentry.browserTracingIntegration({ router })]
+    });
+  }
+}
+
 export function registerPlugins(app: App) {
   loadFonts();
+  loadSentry(app);
   app.use(vuetify).use(fluent).use(router).use(pinia);
 }
